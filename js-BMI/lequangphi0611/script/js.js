c265abef5306b2d7cd06ff388cc9d53f987ebc19
lequangphi0611 <lequangphi0611@gmail.com>
2018-10-15 07:33:56 +0700

    Update js-BMI/lequangphi0611/script/js.js

--- js-BMI/lequangphi0611/script/js.js
+++ js-BMI/lequangphi0611/script/js.js
@@ -1,24 +1,24 @@
-function BMI(weight, height){
-  return weight/(height * height);
+function BMI (weight, height) {
+  return weight / (height * height) ;
 }
 
 
-function phanLoai(bmi){
-  if(bmi < 18.5){
-    return "Thiếu cân";
-  }else if(bmi > 25){
-    return "Thừa cân";
-  }else{
-    return "Cân đối";
+function phanLoai (bmi) {
+  if (bmi < 18.5) {
+    return 'Thiếu cân'
+  } else if (bmi > 25) {
+    return 'Thừa cân'
+  } else {
+    return 'Cân đối'
   }
 }
 
-var show = document.getElementById('show');
-var showScreen = (message) => {//In ra màn hình
-  show.innerHTML = message;
+var show = document.getElementById('show')
+var showScreen = (message) => {      //   In ra màn hình
+  show.innerHTML = message
 }
 
-function getBMI(){
+function getBMI () {
   var weight = document.getElementById('weight').value;
   var height = document.getElementById('height').value;
 
@@ -26,7 +26,7 @@ function getBMI(){
 
   var message = "";//Chứa thông tin hiển thị ra màn hình
   //Nếu người dùng nhập vào dấu ',' hệ thống sẽ chuyển sang dấu '.'
-  if(weight.search(",") > 0){
+  if(weight.search(',') > 0){
     // Dùng String.search() kiểm tra người dùng có nhập dấu ',' vào hay không
     // Lớn hơn 0 vì dấu phẩy ở đầu tiên sẽ k đc tính
     weight = weight.replace(',','.');
@@ -47,7 +47,7 @@ function getBMI(){
       message = "Dữ liệu nhập vào phải lớn hơn 0";
     }else{
       var bmi = BMI(weight , height);
-      message = "Chỉ số BMI "+ bmi + " - " +" Người "+ phanLoai(bmi);
+      message = "Chỉ số BMI "+ bmi.toFixed(2) + " - " +" Người "+ phanLoai(bmi);
     }
   }
   setTimeout(function(){//Dùng setTimout để có time delay
