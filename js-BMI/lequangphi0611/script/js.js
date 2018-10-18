565c13cb4c799ea91754939532499b25c658b103
lequangphi0611 <lequangphi0611@gmail.com>
2018-10-18 11:16:51 +0700

    phi

--- js-BMI/lequangphi0611/script/js.js
+++ js-BMI/lequangphi0611/script/js.js
@@ -1,9 +1,9 @@
-function BMI (weight, height) {
-  return weight / (height * height) ;
+function BMI(weight, height) {
+  return weight / (height * height);
 }
 
 
-function phanLoai (bmi) {
+function phanLoai(bmi) {
   if (bmi < 18.5) {
     return 'Thiếu cân';
   } else if (bmi > 25) {
@@ -13,44 +13,44 @@ function phanLoai (bmi) {
   }
 }
 
-var show = document.getElementById('show');
-var showScreen = (message)=>{      //   In ra màn hình
+var show = document.getElementBysId('show');
+var showScreen = (message) => { //   In ra màn hình
   show.innerHTML = message;
 };
 
-function getBMI () {
+function getBMI() {
   var weight = document.getElementById('weight').value;
   var height = document.getElementById('height').value;
 
-  showScreen("");//Mỗi lần click thì sẽ reset lại...tạo cảm giác chương trình đg run
+  showScreen(""); //Mỗi lần click thì sẽ reset lại...tạo cảm giác chương trình đg run
 
-  var message = "";//Chứa thông tin hiển thị ra màn hình
+  var message = ""; //Chứa thông tin hiển thị ra màn hình
   //Nếu người dùng nhập vào dấu ',' hệ thống sẽ chuyển sang dấu '.'
-  if(weight.search(',') > 0){
+  if (weight.search(',') > 0) {
     // Dùng String.search() kiểm tra người dùng có nhập dấu ',' vào hay không
     // Lớn hơn 0 vì dấu phẩy ở đầu tiên sẽ k đc tính
-    weight = weight.replace(',','.');
+    weight = weight.replace(',', '.');
     //String.replace() chuyển ',' thành dấu '.' để tiếp tục phép toán
   }
-  if(height.search(',') > 0){
-    height = height.replace(',','.');
+  if (height.search(',') > 0) {
+    height = height.replace(',', '.');
   }
 
-  if(weight == "" || height == ""){
-      message = "Vui lòng nhập đầy đủ dữ liệu";
-  }else if(isNaN(weight) || isNaN(height)){
-    message ="Dữ liệu nhập vào không phải số";
-  }else{
+  if (weight == "" || height == "") {
+    message = "Vui lòng nhập đầy đủ dữ liệu";
+  } else if (isNaN(weight) || isNaN(height)) {
+    message = "Dữ liệu nhập vào không phải số";
+  } else {
     weight = parseFloat(weight);
     height = parseFloat(height);
-    if(weight <= 0 || height <= 0){
+    if (weight <= 0 || height <= 0) {
       message = "Dữ liệu nhập vào phải lớn hơn 0";
-    }else{
-      var bmi = BMI(weight , height);
-      message = "Chỉ số BMI "+ bmi.toFixed(2) + " - " +" Người "+ phanLoai(bmi);
+    } else {
+      var bmi = BMI(weight, height);
+      message = "Chỉ số BMI " + bmi.toFixed(2) + " - " + " Người " + phanLoai(bmi);
     }
   }
-  setTimeout(function(){//Dùng setTimout để có time delay
+  setTimeout(function () { //Dùng setTimout để có time delay
     showScreen(message);
-  },200);
+  }, 200);
 }
